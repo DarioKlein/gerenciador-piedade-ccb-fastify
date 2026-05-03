59365f7d1285d913bb94e76904c833ab34986759
Dario Klein <dariokleinalves@gmail.com>
2026-05-03 05:53:16 -0300

    finished api

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,13 @@ import setoresRoutes from './routes/setores'
 import bairrosRoutes from './routes/bairros'
 import familiasRoutes from './routes/familias'
 
+import cors from '@fastify/cors'
+
 export const app = fastify({ logger: true })
 
+await app.register(cors, {
+  origin: 'http://localhost:4200',
+})
 app.register(responsaveisRoutes, { prefix: '/api/responsaveis' })
 app.register(setoresRoutes, { prefix: '/api/setores' })
 app.register(bairrosRoutes, { prefix: '/api/bairros' })
